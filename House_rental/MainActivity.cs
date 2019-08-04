a3fa58507d87396c60f313840ed3fc833ec68d30
chandanrattu <Chandanrattu.cr@gmail.com>
2019-08-04 16:41:30 -0400

    intial

---
+++ House_rental/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace House_rental
+{
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button signup, signin;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+           signup= FindViewById<Button>(Resource.Id.signUp);
+           signin = FindViewById<Button>(Resource.Id.signIn);
+
+            signup.Click += Signup_act;
+
+
+        }
+
+        private void Signup_act(object sender, EventArgs e)
+        {
+            Intent registerPage = new Intent(this, typeof(Sign_up));
+
+            StartActivity(registerPage);
+        }
+    }
+}
---
+++ House_rental/obj/Debug/90/android/src/md5cd430e737bedbcd7c1c6f08b729f9574/Sign_up.java
@@ -0,0 +1,47 @@
+package md5cd430e737bedbcd7c1c6f08b729f9574;
+
+
+public class Sign_up
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("House_rental.Sign_up, House_rental", Sign_up.class, __md_methods);
+	}
+
+
+	public Sign_up ()
+	{
+		super ();
+		if (getClass () == Sign_up.class)
+			mono.android.TypeManager.Activate ("House_rental.Sign_up, House_rental", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
